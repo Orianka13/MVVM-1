444f0b435d26645ee5c9abeac523532d96e645b9
Olesia <odobrogor@gmail.com>
2021-06-21 17:19:28 +0300

    prepare project

---
+++ MVVM-1/Profile.swift
@@ -0,0 +1,15 @@
+//
+//  Profile.swift
+//  MVVM-1
+//
+//  Created by Олеся Егорова on 21.06.2021.
+//
+
+import Foundation
+
+//создаем модель где будем хранить данные пользователя
+struct Profile {
+    var name: String
+    var secondName: String
+    var age: Int
+}
--- MVVM-1/ViewController.swift
+++ MVVM-1/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var secondName: UILabel!
+    @IBOutlet weak var age: UILabel!
+    
+    //создадим некого пользователя, информацию о котором помести в наши ярлыки
+    var profile: Profile? {
+        didSet {
+            guard let profile = profile else { return }
+            self.name.text = profile.name
+            self.secondName.text = profile.secondName
+            self.age.text = String(profile.age)
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        profile = Profile(name: "John", secondName: "Smith", age: 33)
     }
-
-
 }
 
